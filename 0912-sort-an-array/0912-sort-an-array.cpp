c958b8f6fdc758fcac824be43c5fc2a2b8d5ab22
Shashank Shekhar <shashankshekhar31102006@gmail.com>
2026-08-12 15:16:33 +0530

    Time: 237 ms (60.47%), Space: 146.6 MB (53.74%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -29,7 +29,7 @@ public:
     }
 
     void mergesort(vector<int>& arr, int start, int end){
-        if(start ==end)
+        if(start >= end)
             return ;
         int mid = start + (end-start)/2;
         mergesort(arr,start,mid);
