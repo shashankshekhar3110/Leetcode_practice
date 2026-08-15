e25a9b0af780430c6d9bee21094f2c0c59e2b553
Shashank Shekhar <shashankshekhar31102006@gmail.com>
2026-08-15 09:52:39 +0530

    Time: 3 ms (44.05%), Space: 10.8 MB (16.55%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        int start =0, end =s.size()-1;
+        while(end > start){
+            if(s[start] != s[end]){
+                return false;
+            }
+                start++;
+                end--;
+            
+        }
+            return true;
+    }
+};
